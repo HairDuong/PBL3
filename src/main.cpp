fc9cc6eaa7ae85b0b7ac6e575d31a2b5f2a466a5
hoangpham0303 <101450859+hoangpham0303@users.noreply.github.com>
2024-10-16 22:39:06 +0700

    mid term

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <Wire.h>
@@ -7,10 +6,11 @@
 #include<ESP32Servo.h>
 #include <time.h>
 
-// Configuration
-const long gmtOffset_sec = 7 * 3600;   // GMT+7
+// Configuration for NTP Time
+const long gmtOffset_sec = 7 * 3600;   
 const int daylightOffset_sec = 0;
 
+// Wi-Fi and MQTT settings
 const char *ssid = "Hoang11";
 const char *password = "1234567890";
 const char *mqtt_broker = "broker.emqx.io";
@@ -22,6 +22,7 @@ const char *topic4 = "esp32/quat/control";
 const char *topic5 = "esp32/maybom/control";
 const char *topic6 = "esp32/fan/mode";
 const char *topic7 = "esp32/servo/control";
+const char *topic8 = "esp32/bongden/control";
 const char *mqtt_username = "hoangpham1";
 const char *mqtt_password = "123456";
 const int mqtt_port = 1883;
@@ -37,7 +38,10 @@ const int mqtt_port = 1883;
 #define FAN_RELAY_PIN 27
 #define PUMP_BUTTON_PIN 4
 #define PUMP_RELAY_PIN 25
-#define MODE_BUTTON_PIN 17 
+#define MODE_BUTTON_PIN 12
+#define BULB_RELAY_PIN 33
+#define BULB_BUTTON_PIN 15
+#define SERVO_BUTTON_PIN 14  
 
 // Global variables
 float temperature = 0.0;
@@ -48,17 +52,19 @@ long duration;
 bool isFanOn = false;
 int lastFanButtonState = HIGH; 
 bool isPumpOn = false;
-int lastPumpButtonState = HIGH;  
+int lastPumpButtonState = HIGH;
+bool isBulbOn = false;
+int lastBulbButtonState = HIGH; 
 unsigned long lastfeeding = 0;
-unsigned long autofeedinginterval = 1000; 
+unsigned long autofeedinginterval = 1000;
 int targetHour = 13;
 int targetMinute = 43;
 int targetSecond = 0;
-int targetSecondclose =0;
-unsigned long lastModeButtonPress = 0; 
-unsigned long lastFanButtonPress = 0;  
+int targetSecondclose = 0;
 const unsigned long debounceTime = 50;
 int lastModeButtonState = HIGH;
+bool isServoAt90 = false;  
+int lastServoButtonState = HIGH;  
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -73,79 +79,52 @@ unsigned long previousSensorMillis = 0;
 unsigned long previousDisplayMillis = 0; 
 const unsigned long updateSensorInterval = 2000; 
 const unsigned long displayInterval = 5000; 
-bool showSensorData = true; 
-
+bool showSensorData = true;
 void callback(char *topic, byte *payload, unsigned int length) {
   String message;
-  for (int i = 0; i < length; i++) {
-    message += (char)payload[i];
-  }
+  for (int i = 0; i < length; i++) message += (char)payload[i];
 
-  if (String(topic) == topic4) {
-    isFanOn = message == "ON";
-    digitalWrite(FAN_RELAY_PIN, isFanOn ? HIGH : LOW);
-  }
-  if (String(topic) == topic5) {
-    isPumpOn = message == "ON";
-    digitalWrite(PUMP_RELAY_PIN, isPumpOn ? HIGH : LOW);
-  }
-  if (String(topic) == topic7)
-  {
-    if (message== "ON")
-    { myServo.write(90);}
-    if (message == "OFF")
-    { myServo.write(0);}
-    
-  }
+  if (String(topic) == topic4) isFanOn = (message == "ON");
+  if (String(topic) == topic5) isPumpOn = (message == "ON");
+  if (String(topic) == topic7) myServo.write(message == "ON" ? 90 : 0);
+  if (String(topic) == topic8) isBulbOn = (message == "ON");
+
+  digitalWrite(FAN_RELAY_PIN, isFanOn ? HIGH : LOW);
+  digitalWrite(PUMP_RELAY_PIN, isPumpOn ? HIGH : LOW);
+  digitalWrite(BULB_RELAY_PIN, isBulbOn ? HIGH : LOW);
 }
-
-void automaticfeeding() 
-{
+void automaticfeeding() {
   struct tm timeinfo;
-  // Lấy thời gian thực
   if (!getLocalTime(&timeinfo)) {
     Serial.println("Không thể lấy thời gian từ NTP Server");
     return;
   }
-
-  // In ra thời gian hiện tại
   Serial.println(&timeinfo, "Thời gian hiện tại: %H:%M:%S");
-
-  // Kiểm tra xem có đúng thời gian điều khiển servo không
   if (timeinfo.tm_hour == targetHour && timeinfo.tm_min == targetMinute && timeinfo.tm_sec == targetSecond) 
   {
-    
-
-    // Nếu servo chưa ở góc 90 và chưa ghi nhận thời điểm xoay
     if (myServo.read() != 90) 
     {
       Serial.println("Đúng giờ! Điều khiển servo đến góc 90 độ.");
       myServo.write(90);
-      
     }
   }
-    // Kiểm tra nếu đã đủ 10 giây  kể từ khi servo xoay đến góc 90 độ
      if (timeinfo.tm_hour == targetHour && timeinfo.tm_min == targetMinute && timeinfo.tm_sec == (targetSecondclose)) {
      {
       Serial.println("Servo đã ở góc 90 độ trong 10 giây. Quay lại góc 0 độ.");
       myServo.write(0);  // Xoay servo về góc 0 độ
-      
      }
+  }
 }
-}
-
 void setup() {
-  Serial.begin(115200);
-
+  Serial.begin(9600);
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT_PULLDOWN);
   dht.begin();
-
+  pinMode(SERVO_BUTTON_PIN, INPUT_PULLUP); 
   myServo.attach(servoPin);
   configTime(gmtOffset_sec, daylightOffset_sec, "pool.ntp.org", "time.nist.gov");
   myServo.write(0);
   automaticfeeding();
-
   if (!display.begin(0x3C, true)) {
   Serial.println("OLED initialization failed");
   while (1);
@@ -169,7 +148,6 @@ void setup() {
     Serial.println("Connecting to WiFi..");
   }
   Serial.println("Connected to the WiFi network");
-
   client.setServer(mqtt_broker, mqtt_port);
   client.setCallback(callback);
   while (!client.connected()) {
@@ -183,9 +161,9 @@ void setup() {
   }
   client.subscribe(topic4);
   client.subscribe(topic5);
-  client.subcribe(topic7);
+  client.subscribe(topic7);
+  client.subscribe(topic8);
 }
-
 String readUltrasonicSensor() {
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
@@ -197,59 +175,58 @@ String readUltrasonicSensor() {
   distance = (duration * 0.034) / 2;
   return String(distance);
 }
-
+void handleServoControl() {
+  int currentServoButtonState = digitalRead(SERVO_BUTTON_PIN); 
+  if (currentServoButtonState == LOW && lastServoButtonState == HIGH) {
+    delay(50);
+    if (digitalRead(SERVO_BUTTON_PIN) == LOW) { 
+      isServoAt90 = !isServoAt90;
+      if (isServoAt90) {
+        myServo.write(90);  
+      } else {
+        myServo.write(0);   
+      }
+      client.publish(topic7, isServoAt90 ? "ON" : "OFF");
+    }
+  }
+  lastServoButtonState = currentServoButtonState;
+}
 void handleFanControl() {
-  // Switch case for manual and automatic mode
   switch (currentMode) {
     case MANUAL: {
-      int fanButtonState = digitalRead(FAN_BUTTON_PIN); // Read the fan button state
+      int fanButtonState = digitalRead(FAN_BUTTON_PIN); 
       if (fanButtonState == LOW && lastFanButtonState == HIGH) {
-        delay(50); // Simple debounce
+        delay(50); 
         if (digitalRead(FAN_BUTTON_PIN) == LOW) {
-          isFanOn = !isFanOn;  // Toggle fan state
-          digitalWrite(FAN_RELAY_PIN, isFanOn ? HIGH : LOW);  // Control relay
-          Serial.println(isFanOn ? "Fan ON (Manual)" : "Fan OFF (Manual)");  // Debugging output
-          
-          // Publish fan state to MQTT
+          isFanOn = !isFanOn;  
+          digitalWrite(FAN_RELAY_PIN, isFanOn ? HIGH : LOW);  
           client.publish(topic4, isFanOn ? "ON" : "OFF");
         }
       }
-      lastFanButtonState = fanButtonState; // Store the button state for comparison
+      lastFanButtonState = fanButtonState; 
       break;
     }
-
     case AUTOMATIC: {
-      // Automatic fan control based on temperature
       if (temperature > 32.0) {
-        isFanOn = true;  // Turn on the fan if the temperature is higher than 32°C
+        isFanOn = true;  
       } else {
-        isFanOn = false; // Turn off the fan otherwise
+        isFanOn = false; 
       }
-
-      digitalWrite(FAN_RELAY_PIN, isFanOn ? HIGH : LOW);  // Control the relay
-      Serial.println(isFanOn ? "Fan ON (Auto)" : "Fan OFF (Auto)");  // Debugging output
-      
-      // Publish the fan state to MQTT
+      digitalWrite(FAN_RELAY_PIN, isFanOn ? HIGH : LOW);  
       client.publish(topic4, isFanOn ? "ON" : "OFF");
       break;
     }
   }
-
-  // Handle mode switch (manual/automatic) with simple debounce
-  int modeButtonState = digitalRead(MODE_BUTTON_PIN); // Read the mode button state
+  int modeButtonState = digitalRead(MODE_BUTTON_PIN); 
   if (modeButtonState == LOW && lastModeButtonState == HIGH) {
-    delay(50);  // Simple debounce
+    delay(50);  
     if (digitalRead(MODE_BUTTON_PIN) == LOW) {
-      currentMode = (currentMode == MANUAL) ? AUTOMATIC : MANUAL; // Toggle the mode
-      Serial.println(currentMode == MANUAL ? "Mode: Manual" : "Mode: Automatic");
-
-      // Optional: publish the mode change via MQTT if needed
+      currentMode = (currentMode == MANUAL) ? AUTOMATIC : MANUAL; 
       client.publish(topic6, currentMode == MANUAL ? "Manual" : "Automatic");
     }
   }
-  lastModeButtonState = modeButtonState;  // Store the button state for comparison
+  lastModeButtonState = modeButtonState; 
 }
-
 void handlePumpControl() {
   int PumpButtonState = digitalRead(PUMP_BUTTON_PIN);
   if (PumpButtonState == LOW && lastPumpButtonState == HIGH) {
@@ -257,21 +234,29 @@ void handlePumpControl() {
     if (digitalRead(PUMP_BUTTON_PIN) == LOW) {
       isPumpOn = !isPumpOn;
       digitalWrite(PUMP_RELAY_PIN, isPumpOn ? HIGH : LOW);
-      Serial.println(isPumpOn ? "Pump ON" : "Pump OFF");
-      
-      // Gửi trạng thái máy bơm lên MQTT
+
       client.publish(topic5, isPumpOn ? "ON" : "OFF");
     }
   }
   lastPumpButtonState = PumpButtonState;
 }
-
+void handleBulbControl() {
+  int BulbButtonState = digitalRead(BULB_BUTTON_PIN);
+  if (BulbButtonState == LOW && lastBulbButtonState == HIGH) {
+    delay(50);
+    if (digitalRead(BULB_BUTTON_PIN) == LOW) {
+      isBulbOn = !isBulbOn;
+      digitalWrite(BULB_RELAY_PIN, isBulbOn ? HIGH : LOW);
+      client.publish(topic8, isBulbOn ? "ON" : "OFF");
+    }
+  }
+  lastBulbButtonState = BulbButtonState;
+}
 String ratefood (){
   float distanceOrigin = 8.0;
   float foodAvailable = (1.0- (distance/distanceOrigin))*100.0;
   return String(foodAvailable);
 }
-
 void updateStatusDisplay() {
   unsigned long currentMillis = millis();
   if (currentMillis - previousSensorMillis >= updateSensorInterval) {
@@ -287,9 +272,7 @@ void updateStatusDisplay() {
     client.publish(topic2, String(airQuality).c_str());
     client.publish(topic3, rate.c_str());
   }
-
-  if (currentMillis - lastfeeding >= autofeedinginterval) 
-  {
+  if (currentMillis - lastfeeding >= autofeedinginterval) {
     lastfeeding = currentMillis;
     automaticfeeding();
   }
@@ -298,12 +281,11 @@ void updateStatusDisplay() {
     previousDisplayMillis = currentMillis;
     showSensorData = !showSensorData; // Đảo trạng thái hiển thị
   }
-
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(SH110X_WHITE);
-
   display.setCursor(0, 0);
+  
   if (showSensorData) {
     display.println("Sensor Data:");
     display.println("Temp: " + String(temperature) + " C");
@@ -313,16 +295,17 @@ void updateStatusDisplay() {
   } else {
     display.println("Device Status:");
     display.println(isFanOn ? "Fan: ON" : "Fan: OFF");
-    display.println(currentMode ? "Mode: AUTOMATIC " : "Mode: MANUAL");
+    display.println(currentMode ? "Fan mode: AUTOMATIC " : "Fan mode: MANUAL");
     display.println(isPumpOn ? "Pump: ON" : "Pump: OFF");
+    display.println(isBulbOn ? "Bulb: ON" : "Bulb: OFF");
+    display.println(isServoAt90 ? "Servo: ON" : "Servo: OFF");
   }
-
   display.display();
 }
-
 void loop() {
   handleFanControl();
   handlePumpControl();
+  handleServoControl();
   updateStatusDisplay();
   client.loop();
 }
